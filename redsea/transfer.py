4feea517fc4f1223e27dd686a2a98340ba4d8d89
Sebastian Opalczynski <opalczynski@gmail.com>
2016-02-05 12:07:12 +0100

    [SYNCANO] add batching to insert objects;

--- redsea/transfer.py
+++ redsea/transfer.py
@@ -47,9 +47,11 @@ class SyncanoTransfer(object):
             self.data.add_class(syncano_name=class_name, syncano_schema=syncano_schema,
                                 parse_name=parse_schema['className'], parse_schema=parse_schema)
             try:
+                print(syncano_schema)
                 instance.classes.create(name=class_name, schema=syncano_schema)
-            except:
+            except Exception as e:
                 log.error('Class already defined in this instance: {}/{}'.format(class_name, instance.name))
+                log.error(e.message)
 
     def transfer_objects(self, instance):
         for class_to_process in self.data.sort_classes():
@@ -62,11 +64,26 @@ class SyncanoTransfer(object):
                     break
                 limit += PARSE_PAGINATION_LIMIT
                 skip += PARSE_PAGINATION_LIMIT
+                objects_to_add = []
+                parse_ids = []
                 for object in objects['results']:
                     s_class = instance.classes.get(name=class_to_process.syncano_name)
                     syncano_object = ClassAttributeMapper.process_object(object, self.data.reference_map)
-                    created_syncano_object = s_class.objects.create(**syncano_object)
-                    self.data.reference_map[object['objectId']] = created_syncano_object.id
+
+                    if len(objects_to_add) == 10:
+                        created_objects = s_class.objects.batch(
+                            *objects_to_add
+                        )
+
+                        for parse_id, syncano_id in zip(parse_ids, [o.id for o in created_objects]):
+                            self.data.reference_map[parse_id] = syncano_id
+
+                        objects_to_add = []
+                        parse_ids = []
+
+                    batched_syncano_object = s_class.objects.as_batch().create(**syncano_object)
+                    objects_to_add.append(batched_syncano_object)
+                    parse_ids.append(object['objectId'])
 
     def through_the_red_sea(self):
         instance = self.get_syncano_instance()
