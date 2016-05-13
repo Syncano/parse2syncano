a3df24cbaa77497df4d7a21b518c8f0195cc2640
Sebastian Opałczyński <opalczynski@gmail.com>
2016-05-13 12:17:40 +0200

    [RELEASE] correct .pypirc path

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed "s/<USER>/$PYPI_USER/;s/<PASSWORD>/$PYPI_PASSWORD/" < ~/syncano-python/.pypirc.template > ~/.pypirc
+sed "s/<USER>/$PYPI_USER/;s/<PASSWORD>/$PYPI_PASSWORD/" < ~/parse2syncano/.pypirc.template > ~/.pypirc
 python setup.py register -r pypi
 python setup.py sdist upload -r pypi
